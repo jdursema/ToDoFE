e3e3e9128e8d502ec244a26ace7c6d22017fe024
Julie Dursema <jmdursema@gmail.com>
2018-05-30 01:09:04 -0600

    style card

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ({project})=> {
   let mappedTasks = project.tasks.map((task)=> {
     return (
-      <div>
-        <button>X</button>
-        <h3>{task.name}</h3>
+      <div className='task'>
+        <button className='check-btn'></button>
+        <h4 className='task-name'>{task.name}</h4>
       </div>
       )
   })
-  
+
   return(
-   <div>
-     <h2 >{project.name}</h2>
+   <div className='card'>
+     <h2>{project.name}</h2>
+     <hr/>
      {mappedTasks}
    </div>
   )
